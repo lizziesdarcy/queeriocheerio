bb8eea963dccfaf6abb365594f3a526ad78efe2b
lizziesdarcy <ohishax@gmail.com>
2024-11-26 17:10:47 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,108 +1,108 @@
 import { Label } from './types.js';
 
-export const DELETE = 'insert-rkey-of-delete-post-here';
+export const DELETE = '3lbtww7zqwd2f';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww4j7al2y',
     identifier: 'lesbian',
     locales: [
       { lang: 'en', name: 'lesbian', description: 'lesbian'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww4q7n32v',
     identifier: 'gay',
     locales: [
       { lang: 'en', name: 'gay', description: 'gay'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww4xfxa2m',
     identifier: 'bisexual',
     locales: [
       { lang: 'en', name: 'bisexual', description: 'bisexual'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww56hjy2m',
     identifier: 'transgender',
     locales: [
       { lang: 'en', name: 'transgender', description: 'transgender'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww5fla32v',
     identifier: 'transsexual',
     locales: [
       { lang: 'en', name: 'transsexual', description: 'transsexual'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww5mke72x',
     identifier: 'queer',
     locales: [
       { lang: 'en', name: 'queer', description: 'queer'},
     ]
   },
 {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww5tcar2a',
     identifier: 'questioning',
     locales: [
       { lang: 'en', name: 'questioning', description: 'questioning'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww62fal2v',
     identifier: 'intersex',
     locales: [
       { lang: 'en', name: 'intersex', description: 'intersex'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww6bfu32v',
     identifier: 'ace-spec',
     locales: [
       { lang: 'en', name: 'ace-spec', description: 'ace-spec'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww6igp72j',
     identifier: 'aro-spec',
     locales: [
       { lang: 'en', name: 'aro-spec', description: 'aro-spec'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww6ph7m27',
     identifier: 'agender',
     locales: [
       { lang: 'en', name: 'agender', description: 'agender'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww6wivh2j',
     identifier: 'aroace',
     locales: [
       { lang: 'en', name: 'aroace', description: 'aroace'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww75kpp2x',
     identifier: 'abrosexual',
     locales: [
       { lang: 'en', name: 'abrosexual', description: 'abrosexual'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww7ek7l2f',
     identifier: 'pansexual',
     locales: [
       { lang: 'en', name: 'pansexual', description: 'pansexual'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lbtww7lkrd2m',
     identifier: 'polysexual',
     locales: [
       { lang: 'en', name: 'polysexual', description: 'polysexual'},
@@ -110,7 +110,7 @@ export const LABELS: Label[] = [
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'omnisexual',
+    identifier: '3lbtww7sgil2m',
     locales: [
       { lang: 'en', name: 'omnisexual', description: 'omnisexual'},
     ]
