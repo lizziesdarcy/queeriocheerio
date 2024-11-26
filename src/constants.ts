670d2712a0ce0cca8b9e5d0c1dfac2abfa9ada2c
lizziesdarcy <ohishax@gmail.com>
2024-11-26 16:47:09 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -5,42 +5,114 @@ export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
     rkey: 'insert-rkey-here',
-    identifier: 'earth',
+    identifier: 'lesbian',
     locales: [
-      { lang: 'en', name: 'Earth 🌎', description: 'Earth'},
-      { lang: 'pt-BR', name: 'Terra 🌎', description: 'Terra'},
+      { lang: 'en', name: 'lesbian', description: 'lesbian'},
     ]
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'fire',
+    identifier: 'gay',
     locales: [
-      { lang: 'en', name: 'Fire 🔥', description: 'Fire'},
-      { lang: 'pt-BR', name: 'Fogo 🔥', description: 'Fogo'},
+      { lang: 'en', name: 'gay', description: 'gay'},
     ]
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'air',
+    identifier: 'bisexual',
     locales: [
-      { lang: 'en', name: 'Air 💨', description: 'Air'},
-      { lang: 'pt-BR', name: 'Ar 💨', description: 'Ar'},
+      { lang: 'en', name: 'bisexual', description: 'bisexual'},
     ]
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'water',
+    identifier: 'transgender',
     locales: [
-      { lang: 'en', name: 'Water 💧', description: 'Water'},
-      { lang: 'pt-BR', name: 'Água 💧', description: 'Água'},
+      { lang: 'en', name: 'transgender', description: 'transgender'},
     ]
   },
   {
     rkey: 'insert-rkey-here',
-    identifier: 'love',
+    identifier: 'transsexual',
     locales: [
-      { lang: 'en', name: 'Love 💞', description: 'Love'},
-      { lang: 'pt-BR', name: 'Amor 💞', description: 'Amor'},
+      { lang: 'en', name: 'transsexual', description: 'transsexual'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'queer',
+    locales: [
+      { lang: 'en', name: 'queer', description: 'queer'},
+    ]
+  },
+{
+    rkey: 'insert-rkey-here',
+    identifier: 'questioning',
+    locales: [
+      { lang: 'en', name: 'questioning', description: 'questioning'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'intersex',
+    locales: [
+      { lang: 'en', name: 'intersex', description: 'intersex'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'ace-spec',
+    locales: [
+      { lang: 'en', name: 'ace-spec', description: 'ace-spec'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'aro-spec',
+    locales: [
+      { lang: 'en', name: 'aro-spec', description: 'aro-spec'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'agender',
+    locales: [
+      { lang: 'en', name: 'agender', description: 'agender'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'aroace',
+    locales: [
+      { lang: 'en', name: 'aroace', description: 'aroace'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'abrosexual',
+    locales: [
+      { lang: 'en', name: 'abrosexual', description: 'abrosexual'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'pansexual',
+    locales: [
+      { lang: 'en', name: 'pansexual', description: 'pansexual'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'polysexual',
+    locales: [
+      { lang: 'en', name: 'polysexual', description: 'polysexual'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'omnisexual',
+    locales: [
+      { lang: 'en', name: 'omnisexual', description: 'omnisexual'},
     ]
   },
 ];
