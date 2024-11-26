9250d4b02e7985a14f52e76498c5fd2f4fde39b3
lizziesdarcy <189726652+lizziesdarcy@users.noreply.github.com>
2024-11-26 22:14:55 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,185 +1,185 @@
 import { Label } from './types.js';
 
-export const DELETE = '3lbuf5om6hk2u';
+export const DELETE = '3lbufsq7f2s2c';
 export const LABEL_LIMIT = 2;
 export const LABELS: Label[] = [
   {
-    rkey: '3lbuf7z6qlc2u',
+    rkey: '3lbufzddhfl2c',
     identifier: 'theagcd',
     locales: [
       { lang: 'en', name: 'theagcd 🇺🇸', description: 'fsotus alex claremont-diaz'},
     ]
   },
   {
-    rkey: '3lbuf7z6rkk2u',
+    rkey: '3lbufzddiet2c',
     identifier: 'princehenryuk',
     locales: [
       { lang: 'en', name: 'princehenryuk 🇬🇧', description: 'hrh prince henry george edward james fox-mountchristen-windsor'},
     ]
   },
   {
-    rkey: '3lbufdkmqqc2u',
+    rkey: '3lbug2vzrhl2c',
     identifier: 'alex',
     locales: [
       { lang: 'en', name: 'alex 🤠', description: 'acd'},
     ]
   },
   {
-    rkey: '3lbufdkmrpk2u',
+    rkey: '3lbug2vzsgt2c',
     identifier: 'henry',
     locales: [
       { lang: 'en', name: 'prince hrh dickhead 💩', description: 'h fox'},
     ]
   },
   {
-    rkey: '3lbufdkmrpl2u',
+    rkey: '3lbug2vzsgu2c',
     identifier: 'june',
     locales: [
       { lang: 'en', name: 'junebug 🐞', description: 'june claremont-diaz'},
     ]
   },
   {
-    rkey: '3lbufdkmrpm2u',
+    rkey: '3lbug2vzsgv2c',
     identifier: 'nora',
     locales: [
       { lang: 'en', name: 'irl chaos demon 📊', description: 'nora holleran'},
     ]
   },
 {
-    rkey: '3lbufdkmrpn2u',
+    rkey: '3lbug2vztg52c',
     identifier: 'pez',
     locales: [
       { lang: 'en', name: 'pez like sweets 🍬', description: 'percy okonjo'},
     ]
   },
   {
-    rkey: '3lbufdkmsov2u',
+    rkey: '3lbug2vztg62c',
     identifier: 'bea',
     locales: [
       { lang: 'en', name: 'qween bea 🐝', description: 'hrh princess beatrice'},
     ]
   },
   {
-    rkey: '3lbufkaskyv2u',
+    rkey: '3lbugbe6gag2c',
     identifier: 'david',
     locales: [
       { lang: 'en', name: 'david the beagle 🐶', description: 'hrh prince david'},
     ]
   },
   {
-    rkey: '3lbufkaskyv2u',
+    rkey: '3lbugbe6gah2c',
     identifier: 'mr wobbles',
     locales: [
       { lang: 'en', name: 'mr wobbles 🐈‍⬛', description: 'mr wobbles'},
     ]
   },
   {
-    rkey: '3lbufkaskyv2u',
+    rkey: '3lbugbe6h7p2c',
     identifier: 'cornbread',
     locales: [
       { lang: 'en', name: 'cornbread 🦃', description: 'turkey 1'},
     ]
   },
   {
-    rkey: '3lbufkaskyv2u',
+    rkey: '3lbugbe6h7q2c',
     identifier: 'stuffing',
     locales: [
       { lang: 'en', name: 'stuffing 🦃', description: 'turkey 2'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbufwxtbzc2c',
     identifier: 'book',
     locales: [
       { lang: 'en', name: 'rwrb 🩷', description: 'red, white & royal blue'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbufwxtcyk2c',
     identifier: 'collectors',
     locales: [
       { lang: 'en', name: 'rwrbonus 🩵', description: 'red, white & royal blue (collectors edition)'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbufwxtdxs2c',
     identifier: 'movie',
     locales: [
       { lang: 'en', name: 'rwrbmovie 🎞️', description: 'red, white & royal blue (2023)'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbufwxtdxt2c',
     identifier: 'sequel',
     locales: [
       { lang: 'en', name: 'rwrb2 🍰', description: 'red, white & royal blue sequel'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbugvejmyi2c',
     identifier: 'ring',
     locales: [
       { lang: 'en', name: '💍', description: 'henry'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbugvejlza2c',
     identifier: 'key',
     locales: [
       { lang: 'en', name: '🗝️', description: 'alex'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbugvejnxq2c',
     identifier: 'note',
     locales: [
       { lang: 'en', name: 'dear thisbe 📜', description: 'the note™️'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbugvejnxr2c',
     identifier: 'j14',
     locales: [
       { lang: 'en', name: 'j14 magazine', description: 'alex'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbugvejnxs2c',
     identifier: 'le monde',
     locales: [
       { lang: 'en', name: 'le monde', description: 'henry'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbuhhir64s2c',
     identifier: 'paris?',
     locales: [
       { lang: 'en', name: 'paris? 📨', description: 'email 1'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbuhhir7422c',
     identifier: 'you are a dark sorcerer',
     locales: [
       { lang: 'en', name: 'you are a dark sorcerer 📨', description: 'email 2'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbuhhir7432c',
     identifier: 'a mass of fools and knaves',
     locales: [
       { lang: 'en', name: 'a mass of fools and knaves 📨', description: 'email 3'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbuhhir7442c',
     identifier: 'hometown stuff',
     locales: [
       { lang: 'en', name: 'hometown stuff 📨', description: 'email 4'},
     ]
   },
   {
-    rkey: '',
+    rkey: '3lbuhhira3e2c',
     identifier: 'bad metaphors about maps',
     locales: [
       { lang: 'en', name: 'bad metaphors about maps 📨', description: 'email 5'},
