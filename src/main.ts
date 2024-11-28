6167d4ca544eda8df7157fd630adb4503e719c80
lizziesdarcy <189726652+lizziesdarcy@users.noreply.github.com>
2024-11-28 16:41:49 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { CommitCreateEvent, Jetstream } from '@skyware/jetstream';
 import fs from 'node:fs';
+
 import { CURSOR_UPDATE_INTERVAL, DID, FIREHOSE_URL, HOST, METRICS_PORT, PORT, WANTED_COLLECTION } from './config.js';
 import { label, labelerServer } from './label.js';
 import logger from './logger.js';
@@ -13,11 +14,13 @@ function epochUsToDateTime(cursor: number): string {
 }
 
 try {
-  logger.info('Starting labeler service...');
+  logger.info('Trying to read cursor from cursor.txt...');
   cursor = Number(fs.readFileSync('cursor.txt', 'utf8'));
+  logger.info(`Cursor found: ${cursor} (${epochUsToDateTime(cursor)})`);
 } catch (error) {
   if (error instanceof Error && 'code' in error && error.code === 'ENOENT') {
     cursor = Math.floor(Date.now() * 1000);
+    logger.info(`Cursor not found in cursor.txt, setting cursor to: ${cursor} (${epochUsToDateTime(cursor)})`);
     fs.writeFileSync('cursor.txt', cursor.toString(), 'utf8');
   } else {
     logger.error(error);
@@ -32,9 +35,12 @@ const jetstream = new Jetstream({
 });
 
 jetstream.on('open', () => {
-  logger.info('Jetstream connected');
+  logger.info(
+    `Connected to Jetstream at ${FIREHOSE_URL} with cursor ${jetstream.cursor} (${epochUsToDateTime(jetstream.cursor!)})`,
+  );
   cursorUpdateInterval = setInterval(() => {
     if (jetstream.cursor) {
+      logger.info(`Cursor updated to: ${jetstream.cursor} (${epochUsToDateTime(jetstream.cursor)})`);
       fs.writeFile('cursor.txt', jetstream.cursor.toString(), (err) => {
         if (err) logger.error(err);
       });
@@ -42,9 +48,18 @@ jetstream.on('open', () => {
   }, CURSOR_UPDATE_INTERVAL);
 });
 
+jetstream.on('close', () => {
+  clearInterval(cursorUpdateInterval);
+  logger.info('Jetstream connection closed.');
+});
+
+jetstream.on('error', (error) => {
+  logger.error(`Jetstream error: ${error.message}`);
+});
+
 jetstream.onCreate(WANTED_COLLECTION, (event: CommitCreateEvent<typeof WANTED_COLLECTION>) => {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   if (event.commit?.record?.subject?.uri?.includes(DID)) {
-    logger.info('Label request from:', event.did);
     label(event.did, event.commit.record.subject.uri.split('/').pop()!);
   }
 });
@@ -53,28 +68,26 @@ const metricsServer = startMetricsServer(METRICS_PORT);
 
 labelerServer.app.listen({ port: PORT, host: HOST }, (error, address) => {
   if (error) {
-    logger.error('Server error:', error);
+    logger.error('Error starting server: %s', error);
   } else {
-    logger.info('Labeler server started');
+    logger.info(`Labeler server listening on ${address}`);
   }
 });
 
 jetstream.start();
 
-process.on('SIGINT', () => {
-  logger.info('Shutting down...');
-  fs.writeFileSync('cursor.txt', jetstream.cursor!.toString(), 'utf8');
-  jetstream.close();
-  labelerServer.stop();
-  metricsServer.close();
-  process.exit(0);
-});
+function shutdown() {
+  try {
+    logger.info('Shutting down gracefully...');
+    fs.writeFileSync('cursor.txt', jetstream.cursor!.toString(), 'utf8');
+    jetstream.close();
+    labelerServer.stop();
+    metricsServer.close();
+  } catch (error) {
+    logger.error(`Error shutting down gracefully: ${error}`);
+    process.exit(1);
+  }
+}
 
-process.on('SIGTERM', () => {
-  logger.info('Shutting down...');
-  fs.writeFileSync('cursor.txt', jetstream.cursor!.toString(), 'utf8');
-  jetstream.close();
-  labelerServer.stop();
-  metricsServer.close();
-  process.exit(0);
-});
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
